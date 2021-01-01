8179093ee0d3c43ad58e450e237d5e25f5f02cd7
Sudharshana S L <15.hari.hari@gmail.com>
2021-01-01 15:01:45 +0530

    interpolation and property binding

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -5,4 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+  name = "Second time"
+  wasWatched() {
+    return true
+  }
+}
