06227c6f9a4d160ee50c6d44539b7424e7d8e03b
Sudharshana S L <15.hari.hari@gmail.com>
2021-01-02 10:04:43 +0530

    event listener to directive

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,10 +1,21 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[mwFavorite]'
 })
 export class FavoriteDirective {
   @HostBinding('class.is-favorite') isFavorite = true;
+  @HostBinding('class.is-favorite-hovering') hovering = true;
+  // directives can also be made to listen to host events
+  // angular works with native events without "on" prefix
+  @HostListener('mouseenter') onMouseEnter() {
+    this.hovering = true;
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.hovering = false;
+  }
+
   @Input() set mwFavorite(value) {
     this.isFavorite = value;
   }
