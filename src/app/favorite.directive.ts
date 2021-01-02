d3a1496e323838fb59c562a6f81e29086b0f935a
Sudharshana S L <15.hari.hari@gmail.com>
2021-01-02 09:57:25 +0530

    set directive input

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,8 +1,13 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[mwFavorite]'
 })
 export class FavoriteDirective {
-  @HostBinding('class.is-favorite') isFavorite = true;
+  @HostBinding('class.is-favorite') isFavorite;
+
+  // @Input binding can be added to setter funcion with same name as directive selector 
+  @Input() set mwFavorite(value) {
+    this.isFavorite = value;
+  }
 }
