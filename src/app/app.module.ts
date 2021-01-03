0615e60acf91dcab07d6e78f9c820e8a77eba9e9
Sudharshana S L <15.hari.hari@gmail.com>
2021-01-03 09:57:15 +0530

    template driven form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { MediaItemListComponent } from './media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
 import { MediaItemFormComponent } from './media-item-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    // angular will lookout for any form elements within the templates
+    // using the builtin ngForm directive, but it requires to know which are the form fields
+    // for that, we need to use the ngModel directive on the form fields
+    // otherwise the field will not be included in form group object
+    FormsModule
   ],
   declarations: [
     AppComponent,
