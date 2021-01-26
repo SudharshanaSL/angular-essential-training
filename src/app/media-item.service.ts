d509c1e6fc36b70a4e54e729bd74b85911fa5572
Sudharshana S L <15.hari.hari@gmail.com>
2021-01-26 20:49:40 +0530

    use params for the http get calls

--- src/app/media-item.service.ts
+++ src/app/media-item.service.ts
@@ -6,55 +6,18 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class MediaItemService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  mediaItems = [
-    {
-      id: 1,
-      name: 'Firebug',
-      medium: 'Series',
-      category: 'Science Fiction',
-      year: 2010,
-      watchedOn: 1294166565384,
-      isFavorite: false
-    },
-    {
-      id: 2,
-      name: 'The Small Tall',
-      medium: 'Movies',
-      category: 'Comedy',
-      year: 2015,
-      watchedOn: null,
-      isFavorite: true
-    }, {
-      id: 3,
-      name: 'The Redemption',
-      medium: 'Movies',
-      category: 'Action',
-      year: 2016,
-      watchedOn: null,
-      isFavorite: false
-    }, {
-      id: 4,
-      name: 'Hoopers',
-      medium: 'Series',
-      category: 'Drama',
-      year: null,
-      watchedOn: null,
-      isFavorite: true
-    }, {
-      id: 5,
-      name: 'Happy Joe: Cheery Road',
-      medium: 'Movies',
-      category: 'Action',
-      year: 2015,
-      watchedOn: 1457166565384,
-      isFavorite: false
-    }
-  ];
+  mediaItems = [];
 
-  get() {
-    return this.http.get<MediaItemsResponse>('mediaitems')
+  get(medium) {
+    // passing option params for get call
+    const getOptions = {
+      params: {
+        medium
+      }
+    };
+    return this.http.get<MediaItemsResponse>('mediaitems', getOptions)
       .pipe(
         map((response: MediaItemsResponse) => {
           return response.mediaItems;
