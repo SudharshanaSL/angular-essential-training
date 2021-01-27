e19c86874bc0c7335afd6ae8e66ca07f9f008d69
Sudharshana S L <15.hari.hari@gmail.com>
2021-01-27 20:55:00 +0530

    lazy loading

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,8 +2,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { MediaItemListComponent } from './media-item-list.component';
 
 const appRoutes: Routes = [
+  {
+    path: 'add',
+    // to lazy load another module
+    // make sure to handle the routing in feature module accordingly
+    loadChildren: () => import('./new-item/new-item.module').then((m) => m.NewItemModule)
+  },
   { path: ':medium', component: MediaItemListComponent },
   { path: '', pathMatch: 'full', redirectTo: 'all' }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
+
